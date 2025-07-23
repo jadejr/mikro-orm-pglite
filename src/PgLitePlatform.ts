ac77ce6d336487e383e76c4bdfc0cb56e63fe2df
Johnny Robeson <johnny@localmomentum.net>
2025-07-23 03:09:56 -0400

    refactor: Drop now unnecessary PgLiteSchemaHelper
    The upstream postgres schema helper now works with postgres drivers that return arrays.
    
    https://github.com/mikro-orm/mikro-orm/pull/6501

--- src/PgLitePlatform.ts
+++ src/PgLitePlatform.ts
@@ -1,12 +1,11 @@
 import { type MikroORM } from '@mikro-orm/core';
-import { PostgreSqlPlatform } from '@mikro-orm/postgresql';
+import { PostgreSqlPlatform, PostgreSqlSchemaHelper } from '@mikro-orm/postgresql';
 
 import { PgLiteExceptionConverter } from './PgLiteExceptionConverter.js';
 import { PgLiteSchemaGenerator } from './PgLiteSchemaGenerator.js';
-import { PgLiteSchemaHelper } from './PgLiteSchemaHelper.js';
 
 export class PgLitePlatform extends PostgreSqlPlatform {
-  protected override readonly schemaHelper: PgLiteSchemaHelper = new PgLiteSchemaHelper(this);
+  protected override readonly schemaHelper: PostgreSqlSchemaHelper = new PostgreSqlSchemaHelper(this);
   protected override readonly exceptionConverter = new PgLiteExceptionConverter();
 
   override lookupExtensions(orm: MikroORM): void {
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from '@mikro-orm/knex';
 export * from './PgLiteConnection.js';
 export * from './PgLiteDriver.js';
 export * from './PgLitePlatform.js';
-export * from './PgLiteSchemaHelper.js';
 export * from './PgLiteExceptionConverter.js';
 export * from './types/index.js';
 export {
@@ -12,6 +11,8 @@ export {
 } from './PgLiteMikroORM.js';
 
 // compatibility exports
+export { PostgreSqlSchemaHelper as PgLiteSchemaHelper } from '@mikro-orm/postgresql';
+export { PostgreSqlSchemaHelper } from '@mikro-orm/postgresql';
 export { PgLiteDriver as PostgreSqlDriver } from './PgLiteDriver.js';
 export { PgLiteConnection as PostgreSqlConnection } from './PgLiteConnection.js';
 export { PgLitePlatform as PostgreSqlPlatform } from './PgLitePlatform.js';
