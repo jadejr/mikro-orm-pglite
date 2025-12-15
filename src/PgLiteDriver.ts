140aaf4685e42db2c738ecdde717142176be3e5f
Johnny Robeson <johnny@localmomentum.net>
2025-12-15 04:48:01 -0500

    refactor: Provide getORMClass to ease discovery by next ver of @mikro-orm/nextjs

--- src/PgLiteDriver.ts
+++ src/PgLiteDriver.ts
@@ -1,12 +1,18 @@
-import type { Configuration } from '@mikro-orm/core';
+import { type Configuration, type Constructor } from '@mikro-orm/core';
 import { AbstractSqlDriver } from '@mikro-orm/sql';
 
 import { PgLiteConnection } from './PgLiteConnection.js';
+import { PgLiteMikroORM } from './PgLiteMikroORM.js';
 import { PgLitePlatform } from './PgLitePlatform.js';
 
 export class PgLiteDriver extends AbstractSqlDriver<PgLiteConnection> {
   constructor(config: Configuration) {
     super(config, new PgLitePlatform(), PgLiteConnection, ['kysely', '@electric-sql/pglite']);
+  }
+
+  /** @inheritDoc */
+  override getORMClass(): Constructor<PgLiteMikroORM> {
+    return PgLiteMikroORM;
   }
 
   /*
