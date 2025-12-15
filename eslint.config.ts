c88f302d65235b3a7aab7a40098803b0859f0415
Johnny Robeson <johnny@localmomentum.net>
2025-12-15 05:48:05 -0500

    chore: Add globalIgnores to eslint.config.ts

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,33 +1,34 @@
 import eslint from '@eslint/js';
-import { defineConfig } from 'eslint/config';
+import { defineConfig, globalIgnores } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 
-//
-//...eslintPluginPrettier
-export default defineConfig({
-  files: ['src/**/*.ts', 'eslint.config.ts'],
-  extends: [eslint.configs.recommended, tseslint.configs.recommendedTypeChecked as any, eslintPluginPrettier],
-  languageOptions: {
-    parserOptions: {
-      sourceType: 'module',
-      project: './tsconfig.json',
-      tsconfigRootDir: __dirname,
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['src/**/*.ts', 'eslint.config.ts'],
+    extends: [eslint.configs.recommended, tseslint.configs.recommendedTypeChecked as any, eslintPluginPrettier],
+    languageOptions: {
+      parserOptions: {
+        sourceType: 'module',
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+      'prettier/prettier': 'warn',
     },
   },
-  rules: {
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': [
-      'warn',
-      {
-        vars: 'all',
-        args: 'after-used',
-        ignoreRestSiblings: false,
-        varsIgnorePattern: '^_',
-        argsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_',
-      },
-    ],
-    'prettier/prettier': 'warn',
-  },
-});
+]);
