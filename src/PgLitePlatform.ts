345139558b8d2eee6e5f83ecd311a134b2d03efc
Johnny Robeson <johnny@localmomentum.net>
2025-12-15 04:26:14 -0500

    refactor: Drop exception converter and SchemaHelper exports

--- src/PgLitePlatform.ts
+++ src/PgLitePlatform.ts
@@ -2,12 +2,10 @@ import { type MikroORM } from '@mikro-orm/core';
 import { BasePostgreSqlPlatform, PostgreSqlSchemaHelper } from '@mikro-orm/sql';
 
 import { type PgLiteDriver } from './PgLiteDriver.js';
-import { PgLiteExceptionConverter } from './PgLiteExceptionConverter.js';
 import { PgLiteSchemaGenerator } from './PgLiteSchemaGenerator.js';
 
 export class PgLitePlatform extends BasePostgreSqlPlatform {
   protected override readonly schemaHelper: PostgreSqlSchemaHelper = new PostgreSqlSchemaHelper(this);
-  protected override readonly exceptionConverter = new PgLiteExceptionConverter();
 
   /** @inheritDoc */
   override lookupExtensions(orm: MikroORM<PgLiteDriver>): void {
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ export * from '@mikro-orm/sql';
 export * from './PgLiteConnection.js';
 export * from './PgLiteDriver.js';
 export * from './PgLitePlatform.js';
-export * from './PgLiteExceptionConverter.js';
 export {
   PgLiteMikroORM as MikroORM,
   PgLiteOptions as Options,
@@ -10,8 +9,7 @@ export {
 } from './PgLiteMikroORM.js';
 
 // compatibility exports
-export { PostgreSqlSchemaHelper as PgLiteSchemaHelper } from '@mikro-orm/postgresql';
-export { PostgreSqlSchemaHelper } from '@mikro-orm/postgresql';
 export { PgLiteDriver as PostgreSqlDriver } from './PgLiteDriver.js';
 export { PgLiteConnection as PostgreSqlConnection } from './PgLiteConnection.js';
 export { PgLitePlatform as PostgreSqlPlatform } from './PgLitePlatform.js';
+
